60757a3cdaf53ff8a04c5cc2810b6ccef6174866
evuraan <evuraan@gmail.com>
2024-04-04 10:05:14 -0700

    optimization tweaks

--- scripts/playSelectedTTS.sh
+++ scripts/playSelectedTTS.sh
@@ -1,17 +1,17 @@
-#!/bin/bash 
+#!/bin/bash
 
 set -eo pipefail
 
-dir="$(dirname $0)"
-input="/tmp/toplay.$RANDOM-$RANDOM"
-wav="/tmp/toplay.$RANDOM-$RANDOM.wav"
-
 data=$(xclip -out selection)
-if [ ${#data} -gt 2 ]; then
-    echo $data > $input
-    echo "$(date) data: $input" >> /tmp/piper.log &
-    "$dir"/piper --model "$dir"/en_US-amy-low.onnx --output_file $wav < $input &
-    wait
-    aplay $dir/silence.wav $wav
-    rm $input $wav || : 
+if [[ ${#data} -gt 2 ]]; then
+	dir="$(dirname $0)"
+	input="/tmp/toplay.$RANDOM-$RANDOM"
+	wav="$input.wav"
+	trap 'rm -f "$input" "$wav"' EXIT SIGINT
+
+	echo "$data" >"$input"
+	"$dir"/piper --model "$dir"/en_US-amy-low.onnx --output_file "$wav" <"$input" &
+	echo "$(date) data: $input" >>/tmp/piper.log &
+	wait
+	aplay "$dir"/silence.wav "$wav"
 fi
